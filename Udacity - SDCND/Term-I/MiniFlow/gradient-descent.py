2e8c37d543751fa05b61efcb8ac4d24410cdc352
sohonisaurabh <sohonisaurabh@ymail.com>
2017-02-15 18:39:03 +0530

    Added Miniflow python files along with some notes

---
+++ Udacity - SDCND/Term-I/MiniFlow/gradient-descent.py
@@ -0,0 +1,62 @@
+#Two files from exercise were merged here
+# 1. gd.py which contains definition for gradient descent method
+# 2. f.py which contains few method definitions and test case for forward propagation
+
+
+#=====================Code from gd.py goes here========================
+
+def gradient_descent_update(x, gradx, learning_rate):
+    """
+    Performs a gradient descent update.
+    """
+    # TODO: Implement gradient descent.
+    
+    # Return the new value for x
+
+    x = x - (learning_rate*gradx)
+    return x
+
+
+#===========================Code from gd.py ends======================
+
+#=====================Code from f.py goes here========================
+
+"""
+Given the starting point of any `x` gradient descent
+should be able to find the minimum value of x for the
+cost function `f` defined below.
+"""
+import random
+#from gd import gradient_descent_update
+
+
+def f(x):
+    """
+    Quadratic function.
+
+    It's easy to see the minimum value of the function
+    is 5 when is x=0.
+    """
+    return x**2 + 5
+
+
+def df(x):
+    """
+    Derivative of `f` with respect to `x`.
+    """
+    return 2*x
+
+
+# Random number better 0 and 10,000. Feel free to set x whatever you like.
+x = random.randint(0, 10000)
+# TODO: Set the learning rate
+learning_rate = 0.01
+epochs = 100
+
+for i in range(epochs+1):
+    cost = f(x)
+    gradx = df(x)
+    print("EPOCH {}: Cost = {:.3f}, x = {:.3f}".format(i, cost, gradx))
+    x = gradient_descent_update(x, gradx, learning_rate)
+
+#===========================Code from f.py ends======================
