f5f03667cabb5524ceada056c7f88cada26b4eb5
Saurabh Sohoni <sohonisaurabh@ymail.com>
2017-07-02 21:17:52 +0530

    EKF code is complete except normalization

--- Udacity - SDCND/Term-II/projects/Extended_Kalman_Filter/src/FusionEKF.cpp
+++ Udacity - SDCND/Term-II/projects/Extended_Kalman_Filter/src/FusionEKF.cpp
@@ -148,14 +148,17 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Update the state and covariance matrices.
    */
 
-  /*if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+  if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+    ekf_.R_ = R_radar_;
+    Hj_ = tools.CalculateJacobian(ekf_.x_);
+    ekf_.H_ = Hj_;
   } else {
     // Laser updates
     ekf_.R_ = R_laser_;
     ekf_.H_ = H_laser_;
     ekf_.Update(measurement_pack.raw_measurements_);
-  }*/
+  }
 
   // print the output
   cout << "x_ = " << ekf_.x_ << endl;
--- Udacity - SDCND/Term-II/projects/Extended_Kalman_Filter/src/kalman_filter.cpp
+++ Udacity - SDCND/Term-II/projects/Extended_Kalman_Filter/src/kalman_filter.cpp
@@ -50,4 +50,28 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+   
+  MatrixXd h = MatrixXd(z.rows(), z.cols());
+  //state parameters
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+  float sqrt_px2_py2 = sqrt(pow(px, 2) + pow(py, 2));
+  h << sqrt_px2_py2,
+      atan2(py, px),
+      (px * vx + py * vy)/sqrt_px2_py2;
+
+  VectorXd y = z - h * x_;
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd K = P_ * Ht * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = Eigen::MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
