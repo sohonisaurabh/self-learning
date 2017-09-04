e56959a3fd52844939e71973177591d55343691a
Saurabh <sohonisaurabh@ymail.com>
2017-09-04 13:47:00 +0530

    Updated values for process noise to bring RMSE within project rubric

--- Udacity - SDCND/Term-II/projects/Unscented_Kalman_Filter/src/ukf.cpp
+++ Udacity - SDCND/Term-II/projects/Unscented_Kalman_Filter/src/ukf.cpp
@@ -18,10 +18,10 @@ UKF::UKF() {
   use_radar_ = true;
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 10;
+  std_a_ = 2.5;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 10;
+  std_yawdd_ = 2.5;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
