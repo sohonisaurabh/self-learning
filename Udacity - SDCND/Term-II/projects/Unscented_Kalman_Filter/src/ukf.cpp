c44c0915c9fecaa294d3b5fc86d8f4f8d0431146
Saurabh <sohonisaurabh@ymail.com>
2017-09-06 15:48:04 +0530

    Final RMSE and NIS

--- Udacity - SDCND/Term-II/projects/Unscented_Kalman_Filter/src/ukf.cpp
+++ Udacity - SDCND/Term-II/projects/Unscented_Kalman_Filter/src/ukf.cpp
@@ -20,10 +20,10 @@ UKF::UKF() {
   use_radar_ = true;
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 2.25;
+  std_a_ = 2.0;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 2.5;
+  std_yawdd_ = 2.0;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
